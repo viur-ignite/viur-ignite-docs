774971e140829c30018f52c9ccf80acdfdf70516
Sven Eberth <Sven.Eberth@gmail.com>
2016-07-19 11:51:46 +0200

    added js-codeChild

--- sources/js/app.js
+++ sources/js/app.js
@@ -11,9 +11,23 @@
 SyntaxHighlighter.defaults['gutter'] = false;
 $(function() {
 	$('.js-codeMe').each(function() {
-		var html = $(this).outerHTML().replace(' js-codeMe', '').replace('js-codeMe', '')
+		var html = $(this).outerHTML().replace(' js-codeMe', '').replace('js-codeMe', '').replace(' class=""', '').replace('class=""', '')
 		var escaped = $("<div>").text(html).html();
-		console.log(html);
+
+		if($(this).parent().hasClass('sandbox') === true) {
+			$item = $(this).parent();
+		} else  {
+			$item = $(this);
+		}
+
+		$item.after('<pre class="brush: html">'+escaped+'</pre>');
+
+		SyntaxHighlighter.highlight()
+	})
+
+	$('.js-codeChild').each(function() {
+		var html = $(this).html().replace(' js-codeChild', '').replace('js-codeChild', '').replace(' class=""', '').replace('class=""', '')
+		var escaped = $("<div>").text(html).html();
 
 		if($(this).parent().hasClass('sandbox') === true) {
 			$item = $(this).parent();
